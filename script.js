959c957517bed71a2e8dd85a25cb427e31597a45
Andrew Wand <andrewwands@gmail.com>
2021-12-30 01:00:16 -0600

    Added JS add book function and remove function

--- script.js
+++ script.js
@@ -4,76 +4,94 @@ const modalAddBtn = document.getElementById('modalAddBtn');
 const titleInput = document.getElementById('titleInput');
 const authorInput = document.getElementById('authorInput');
 const cardsContainer = document.getElementById('card-container');
+const removeBtn = document.getElementById('removeBtn');
+const singleCard = document.getElementById('singleCard');
+const checkBox = document.getElementById('haveRead');
+const modalForm = document.getElementById('modalForm');
 
-let userTitleInput = titleInput.value;
-let userAuthorInput = authorInput.value;
+
+
+// Book constructor
+function Book(title, author) {
+  this.title = title
+  this.author = author
+}
 
 
 let myLibrary = [];
 
 
-function addBookToLibrary() {
-    const titleName = titleInput.value;
-    const authorName = authorInput.value;
+const form = document.querySelector('form');
 
-    if (titleName.trim() && authorName.trim()) {
-        const newCard = {
-            titleName,
-            authorName
-        };
-        createCard(newCard);
 
-        titleInput.value = '';
-        authorInput.value = '';
-        closeModal();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
+  let modalContainer = document.getElementById('modalContainer')
+  modalContainer.classList.remove('visible');
+
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+  
+
+  let book = new Book(title, author);
+  myLibrary.push(book);
+
+  const cardDiv = document.createElement('div');
+  cardsContainer.appendChild(cardDiv).className = 'card-div';
+  const cardInsideDiv = document.createElement('div');
+  cardDiv.appendChild(cardInsideDiv).className = 'card-inside-div';
+  const titleTag = document.createElement('h2');
+  cardInsideDiv.appendChild(titleTag);
+  const authorTag = document.createElement('p');
+  cardInsideDiv.appendChild(authorTag);
+
+  const deleteBtn = document.createElement('button');
+  cardInsideDiv.appendChild(deleteBtn).className = 'delete-btn';
+  deleteBtn.textContent = 'Remove';
+
+  
+  
+  
+  
+  
+  deleteBtn.addEventListener('click', removeBtn);
+  function removeBtn() {
+    cardDiv.style.display = 'none';
+  }
+
+
+  
+
+
+  function displayCard(array) {
+        
+    for(let x in array) {
+        if(x === 'title') {
+            titleTag.textContent = (array[x]);
+        }
+        else if (x === 'author') {
+            authorTag.textContent = (array[x]);
+        }
     }
 }
 
-function createCard(data, index) {
-    const card = document.createElement('div');
-    card.classList.add('card');
+displayCard(book);
+modalForm.reset();
 
-    if (index === 0) {
-        card.classList.add('active');
-    }
-
-    card.innerHTML = `
-  <div class="book-card">
-    <p>
-      ${data.titleName}
-    </p>
-    <p>
-    ${data.authorName}
-    </p>
-  </div>
-</div>
-  `;
-
-  myLibrary.push(card);
-
-  cardsContainer.appendChild(card);
-}
-
-
-
-
+});
 
 
 
 function modalOpen() {
-    let modalContainer = document.getElementById('modalContainer')
-    modalContainer.classList.add('visible');
+  let modalContainer = document.getElementById('modalContainer')
+  modalContainer.classList.add('visible');
 }
 
 function closeModal() {
-    let modalContainer = document.getElementById('modalContainer')
-    modalContainer.classList.remove('visible');
+  let modalContainer = document.getElementById('modalContainer')
+  modalContainer.classList.remove('visible');
 }
 
-
-
-
-modalAddBtn.addEventListener('click', addBookToLibrary);
 addBookBtn.addEventListener('click', modalOpen);
 cancelBtn.addEventListener('click', closeModal);
